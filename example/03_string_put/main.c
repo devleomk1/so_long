dfd05f89d7a2f5062e5624be91344d33365e9f5f
devleo <wltnskjs@naver.com>
2021-07-06 14:13:56 +0900

    test mlx font size

--- example/03_string_put/main.c
+++ example/03_string_put/main.c
@@ -6,49 +6,21 @@
 /*   By: jisokang <jisokang@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/01 00:33:16 by jisokang          #+#    #+#             */
-/*   Updated: 2021/07/04 21:12:52 by jisokang         ###   ########.fr       */
+/*   Updated: 2021/07/04 21:36:13 by jisokang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include "../../minilibx_mms_20210621/mlx.h"
 
-typedef struct	s_img
-{
-	void		*ptr;
-	int			w;
-	int			h;
-	int			x;
-	int			y;
-}				t_img;
-
-void	*ft_xpm_to_img(void *mlx, char *str, t_img img)
-{
-	return(mlx_png_file_to_image(mlx, str, &img.w, &img.h));
-}
-
-void	ft_put_img_to_win(void *mlx, void *win, t_img img)
-{
-	mlx_put_image_to_window(mlx, win, img.ptr, img.x, img.y);
-}
-
 int	main(void)
 {
 	void	*mlx;
 	void	*win;
 
-	void	*img1;
-	int		img1_width;
-	int		img1_height;
-
-
-	t_img img_png;
-	img_png.x = 0;
-	img_png.y = 0;
-
 	mlx = mlx_init();
-	win = mlx_new_window(mlx, 640, 480, "[MMSbeta] mlx load img");
-	img_png.ptr = mlx_png_file_to_image(mlx, "hero.png", &img_png.w, &img_png.h);
-	mlx_put_image_to_window(mlx, win, img_png.ptr, 140, 100);
+	win = mlx_new_window(mlx, 640, 480, "[MMSbeta] mlx string put");
+	mlx_string_put(mlx, win, 250, 220, 0xFFFFFF, "Hello World!");
+	mlx_string_put(mlx, win, 265, 250, 0x450A06, "YOU DIED");
 	mlx_loop(mlx);
 }
--- minilibx_mms_20210621/mlx_string_put.c
+++ minilibx_mms_20210621/mlx_string_put.c
@@ -27,7 +27,7 @@ void mlx_int_fill(unsigned char *data, int sl)
 	}
       j ++;
     }
-    
+
 }
 
 int mlx_string_put(void *mlx_ptr, void *win_ptr, int x, int y, int color, char *string)
@@ -51,8 +51,10 @@ int mlx_string_put(void *mlx_ptr, void *win_ptr, int x, int y, int color, char *
 
   color = (color&0xFFFFFF)|0xFF000000;
 
-  //  dest_w = (FONT_WIDTH*5)/7;   /// ratio with X11 standard mlx_string_put
-  //  dest_h = (font_atlas.height*5)/7;
+    //dest_w = (FONT_WIDTH*5)/7;   /// ratio with X11 standard mlx_string_put
+    //dest_h = (font_atlas.height*5)/7;
+    //dest_w = (FONT_WIDTH*2);
+    //dest_h = (font_atlas.height*2);
   dest_w = FONT_WIDTH;
   dest_h = font_atlas.height;
   y = y - (dest_h*3)/4;
