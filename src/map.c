695142559a00c6962f2e9f68a20947f03cfa47b7
devleo <wltnskjs@naver.com>
2021-07-12 23:03:42 +0900

    update

--- src/map.c
+++ src/map.c
@@ -6,11 +6,11 @@
 /*   By: jisokang <jisokang@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/11 20:37:55 by jisokang          #+#    #+#             */
-/*   Updated: 2021/07/11 20:49:32 by jisokang         ###   ########.fr       */
+/*   Updated: 2021/07/12 01:40:47 by jisokang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+#include "../include/so_long.h"
 
 int	check_ext(char *str, char *ext)
 {
@@ -18,22 +18,30 @@ int	check_ext(char *str, char *ext)
 	int	j;
 
 	i = 0;
-	j = 0;
+	if (ext[0] != '.')
+		exit_err("Invalid function parameter.\n");
 	while (str[i] != '\0' && str[i] != '.')
 		i++;
 	if (!str[i])
-	{
-		printf("ERROR\n");
-		exit(1);
-	}
+		exit_err("Wrong extension or file path.\n");
+	j = 0;
 	while (str[i + j] != '\0' && ext[j] != '\0')
 	{
-		if (str[i+j] == ext[j])
+		if (str[i + j] == ext[j])
 			j++;
-		break;
+		else
+			break ;
 	}
-	if (str[i + j] == '\0' && ext[j] == '\0')
-		return (1);
-	printf("ERROR\n");
-	exit(1);
+	if (!(str[i + j] == '\0' && ext[j] == '\0'))
+		exit_err("Wrong extension or file path.\n");
+	return (TRUE);
+}
+
+int	main(int argc, char **argv)
+{
+	if (check_ext(argv[1], ".ber"))
+	{
+		printf("Ext Check" GREEN" OK\n"RESET);
+	}
+	return (0);
 }
