68f03c6b9e3233af0ea346ff3de3a0e713986b64
Kang Jisoon <jisokang@student.42seoul.kr>
2021-11-23 17:46:11 +0900

    🐛 Fix 'Reset memory leaks' bug.

--- src/reset_game.c
+++ src/reset_game.c
@@ -6,7 +6,7 @@
 /*   By: jisokang <jisokang@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/06 15:14:05 by jisokang          #+#    #+#             */
-/*   Updated: 2021/08/09 15:17:59 by jisokang         ###   ########.fr       */
+/*   Updated: 2021/11/23 17:41:31 by jisokang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ int	reset_game(t_game *game)
 {
 	ft_putstr_fd("RESET\n", 1);
 	game->flag.game_scene = GAME_PLAYING;
+	free(game->collec.clst);
 	init_collec(game);
 	get_compo_coord(game);
 	init_player(game);
--- src_bonus/reset_game_bonus.c
+++ src_bonus/reset_game_bonus.c
@@ -6,7 +6,7 @@
 /*   By: jisokang <jisokang@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/06 15:14:05 by jisokang          #+#    #+#             */
-/*   Updated: 2021/11/01 23:30:58 by jisokang         ###   ########.fr       */
+/*   Updated: 2021/11/23 17:43:32 by jisokang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ int	reset_game(t_game *game)
 {
 	ft_putstr_fd("RESET\n", 1);
 	game->flag.game_scene = GAME_PLAYING;
+	free(game->collec.clst);
 	init_collec(game);
 	get_compo_coord(game);
 	init_player(game);
